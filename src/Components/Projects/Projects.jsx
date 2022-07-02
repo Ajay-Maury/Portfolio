9150eeac204cccfafe18ac97f46a24215ed0c0e7
Ajay-Maury <ajaymaurya6798@gmail.com>
2022-07-02 08:00:41 +0530

    Project added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -34,7 +34,7 @@ export const Projects = () => {
               <div className='project_videocontainer'>
                 <div>
                   <img
-                    src='https://user-images.githubusercontent.com/97455068/169433809-322cecc7-866a-46cc-a24c-caa63bd48f0e.png'
+                    src='https://user-images.githubusercontent.com/97455068/176983440-fb08f9d9-1379-4d90-8222-affa3ed80864.png'
                     alt=''
                   />
                 </div>
